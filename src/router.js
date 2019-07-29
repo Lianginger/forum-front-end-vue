a8860a83e56a23960be9a4efa95d8a09dac88263
lianginger <jackyliang444@hotmail.com.tw>
2019-07-30 00:14:08 +0800

    Add signin and signup

--- src/router.js
+++ src/router.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NotFound from './views/NotFound.vue'
+import SingIn from './views/SignIn.vue'
+import SignUp from './views/SignUp.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/signin',
+      name: 'sign-in',
+      component: SingIn
+    },
+    {
+      path: '/signup',
+      name: 'sign-up',
+      component: SignUp
+    },
     {
       path: '*',
       name: 'not-found',
