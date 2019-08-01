8b8603dfd490e53f635ab5742059a9413bffc285
lianginger <jackyliang444@hotmail.com.tw>
2019-08-01 23:10:26 +0800

    Set addFavorite and addLike methods

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -16,10 +16,30 @@
         <p class="card-text text-truncate">{{restaurant.description}}</p>
       </div>
       <div class="card-footer">
-        <button type="button" class="btn btn-danger btn-border favorite mr-2">移除最愛</button>
-        <button type="button" class="btn btn-primary btn-border favorite mr-2">加到最愛</button>
-        <button type="button" class="btn btn-danger like mr-2">Unlike</button>
-        <button type="button" class="btn btn-primary like mr-2">Like</button>
+        <button
+          v-if="restaurant.isFavorited"
+          @click.stop.prevent="deleteFavorite"
+          type="button"
+          class="btn btn-danger btn-border favorite mr-2"
+        >移除最愛</button>
+        <button
+          v-else
+          @click.stop.prevent="addFavorite"
+          type="button"
+          class="btn btn-primary btn-border favorite mr-2"
+        >加到最愛</button>
+        <button
+          v-if="restaurant.isLiked"
+          @click.stop.prevent="deleteLike"
+          type="button"
+          class="btn btn-danger like mr-2"
+        >Unlike</button>
+        <button
+          v-else
+          @click.stop.prevent="addLike"
+          type="button"
+          class="btn btn-primary like mr-2"
+        >Like</button>
       </div>
     </div>
   </div>
@@ -36,6 +56,20 @@ export default {
   data() {
     return {
       restaurant: this.initialRestaurant
+    }
+  },
+  methods: {
+    addFavorite() {
+      this.restaurant.isFavorited = true
+    },
+    deleteFavorite() {
+      this.restaurant.isFavorited = false
+    },
+    addLike() {
+      this.restaurant.isLiked = true
+    },
+    deleteLike() {
+      this.restaurant.isLiked = false
     }
   }
 }
