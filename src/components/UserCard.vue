d28c80b4323f1fced4ead3e40b03db0446a4132b
lianginger <jackyliang444@hotmail.com.tw>
2019-08-04 23:58:47 +0800

    Add UserCard

---
+++ src/components/UserCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="col-3">User</div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object
+    }
+  }
+}
+</script>
+
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -2,15 +2,20 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">美食達人</h1>
+    <div class="row">
+      <UserCard v-for="user in 3" :key="user.id" :user="user" />
+    </div>
   </div>
 </template>
 
 <script>
-import NavTabs from "../components/NavTabs";
+import NavTabs from '../components/NavTabs'
+import UserCard from '../components/UserCard'
 
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    UserCard
   }
-};
+}
 </script>
