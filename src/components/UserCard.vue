2053d2cbe59c7f334d1fb9698997c5d160537102
lianginger <jackyliang444@hotmail.com.tw>
2019-08-05 00:30:20 +0800

    Set UserCard to display three users on UserTop page

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="col-3">User</div>
+  <div class="col-3">
+    <a href="#">
+      <img :src="user.image" :alt="user.name" width="140px" height="140px" />
+    </a>
+    <h2>{{user.name}}</h2>
+    <span class="badge badge-secondary">追蹤人數：{{user.FollowerCount}}</span>
+    <p class="mt-3">
+      <button v-if="user.isFollowed" type="button" class="btn btn-danger">取消追蹤</button>
+      <button v-else type="button" class="btn btn-primary">追蹤</button>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     user: {
-      type: Object
+      type: Object,
+      required: true
     }
   }
 }
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -2,8 +2,9 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">美食達人</h1>
-    <div class="row">
-      <UserCard v-for="user in 3" :key="user.id" :user="user" />
+    <hr />
+    <div class="row text-center">
+      <UserCard v-for="user in users" :key="user.id" :user="user" />
     </div>
   </div>
 </template>
@@ -12,10 +13,138 @@
 import NavTabs from '../components/NavTabs'
 import UserCard from '../components/UserCard'
 
+const dummyData = {
+  users: [
+    {
+      id: 3,
+      name: 'user2',
+      email: 'user2@example.com',
+      password: '$2a$10$tsvcnSFsJvdvs2NLm9rW.uYbah93Xl5cTYcQnSeK3sjEopj.NGzk2',
+      isAdmin: false,
+      image: 'https://i.imgur.com/OezkRwO.jpg',
+      createdAt: '2019-07-30T16:24:55.422Z',
+      updatedAt: '2019-08-01T10:34:36.023Z',
+      Followers: [
+        {
+          id: 1,
+          name: 'root',
+          email: 'root@example.com',
+          password:
+            '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
+          isAdmin: true,
+          image: 'https://i.imgur.com/58ImzMM.png',
+          createdAt: '2019-07-30T16:24:54.983Z',
+          updatedAt: '2019-08-01T10:33:51.095Z',
+          Followship: {
+            followerId: 1,
+            followingId: 3,
+            createdAt: '2019-08-01T10:26:55.618Z',
+            updatedAt: '2019-08-01T10:26:55.618Z'
+          }
+        },
+        {
+          id: 2,
+          name: 'user1',
+          email: 'user1@example.com',
+          password:
+            '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
+          isAdmin: false,
+          image: 'https://i.imgur.com/Q14p2le.jpg',
+          createdAt: '2019-07-30T16:24:55.204Z',
+          updatedAt: '2019-08-01T10:34:15.105Z',
+          Followship: {
+            followerId: 2,
+            followingId: 3,
+            createdAt: '2019-08-01T10:27:49.094Z',
+            updatedAt: '2019-08-01T10:27:49.094Z'
+          }
+        }
+      ],
+      FollowerCount: 2,
+      isFollowed: true
+    },
+    {
+      id: 2,
+      name: 'user1',
+      email: 'user1@example.com',
+      password: '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
+      isAdmin: false,
+      image: 'https://i.imgur.com/Q14p2le.jpg',
+      createdAt: '2019-07-30T16:24:55.204Z',
+      updatedAt: '2019-08-01T10:34:15.105Z',
+      Followers: [
+        {
+          id: 1,
+          name: 'root',
+          email: 'root@example.com',
+          password:
+            '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
+          isAdmin: true,
+          image: 'https://i.imgur.com/58ImzMM.png',
+          createdAt: '2019-07-30T16:24:54.983Z',
+          updatedAt: '2019-08-01T10:33:51.095Z',
+          Followship: {
+            followerId: 1,
+            followingId: 2,
+            createdAt: '2019-08-01T10:26:54.947Z',
+            updatedAt: '2019-08-01T10:26:54.947Z'
+          }
+        }
+      ],
+      FollowerCount: 1,
+      isFollowed: true
+    },
+    {
+      id: 1,
+      name: 'root',
+      email: 'root@example.com',
+      password: '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
+      isAdmin: true,
+      image: 'https://i.imgur.com/58ImzMM.png',
+      createdAt: '2019-07-30T16:24:54.983Z',
+      updatedAt: '2019-08-01T10:33:51.095Z',
+      Followers: [
+        {
+          id: 2,
+          name: 'user1',
+          email: 'user1@example.com',
+          password:
+            '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
+          isAdmin: false,
+          image: 'https://i.imgur.com/Q14p2le.jpg',
+          createdAt: '2019-07-30T16:24:55.204Z',
+          updatedAt: '2019-08-01T10:34:15.105Z',
+          Followship: {
+            followerId: 2,
+            followingId: 1,
+            createdAt: '2019-08-01T10:27:45.616Z',
+            updatedAt: '2019-08-01T10:27:45.616Z'
+          }
+        }
+      ],
+      FollowerCount: 1,
+      isFollowed: false
+    }
+  ]
+}
+
 export default {
   components: {
     NavTabs,
     UserCard
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser() {
+      this.users = dummyData.users
+    }
   }
 }
 </script>
