747d06bb88d82aa6c5d562602e29d886f801f767
lianginger <jackyliang444@hotmail.com.tw>
2019-08-21 15:07:41 +0800

    Set add/delete following API in UsersTop page

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -6,18 +6,58 @@
     <h2>{{user.name}}</h2>
     <span class="badge badge-secondary">追蹤人數：{{user.FollowerCount}}</span>
     <p class="mt-3">
-      <button v-if="user.isFollowed" type="button" class="btn btn-danger">取消追蹤</button>
-      <button v-else type="button" class="btn btn-primary">追蹤</button>
+      <button @click.stop.prevent="deleteFollowing(user.id)" v-if="user.isFollowed" type="button" class="btn btn-danger">取消追蹤</button>
+      <button @click.stop.prevent="addFollowing(user.id)" v-else type="button" class="btn btn-primary">追蹤</button>
     </p>
   </div>
 </template>
 
 <script>
+import usersAPI from '../apis/users'
+import { Toast } from '../utils/helpers'
+
 export default {
   props: {
-    user: {
+    initialUser: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      user: this.initialUser
+    }
+  },
+  methods: {
+    async addFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.addFollowing(userId)
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
+        }
+        this.user.isFollowed = true
+        this.user.FollowerCount++
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+      }
+    },
+    async deleteFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFollowing(userId)
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
+        }
+        this.user.isFollowed = false
+        this.user.FollowerCount--
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+      }
     }
   }
 }
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -4,7 +4,7 @@
     <h1 class="mt-5">美食達人</h1>
     <hr />
     <div class="row text-center">
-      <UserCard v-for="user in users" :key="user.id" :user="user" />
+      <UserCard v-for="user in users" :key="user.id" :initial-user="user" />
     </div>
   </div>
 </template>
@@ -12,121 +12,8 @@
 <script>
 import NavTabs from '../components/NavTabs'
 import UserCard from '../components/UserCard'
-
-const dummyData = {
-  users: [
-    {
-      id: 3,
-      name: 'user2',
-      email: 'user2@example.com',
-      password: '$2a$10$tsvcnSFsJvdvs2NLm9rW.uYbah93Xl5cTYcQnSeK3sjEopj.NGzk2',
-      isAdmin: false,
-      image: 'https://i.imgur.com/OezkRwO.jpg',
-      createdAt: '2019-07-30T16:24:55.422Z',
-      updatedAt: '2019-08-01T10:34:36.023Z',
-      Followers: [
-        {
-          id: 1,
-          name: 'root',
-          email: 'root@example.com',
-          password:
-            '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
-          isAdmin: true,
-          image: 'https://i.imgur.com/58ImzMM.png',
-          createdAt: '2019-07-30T16:24:54.983Z',
-          updatedAt: '2019-08-01T10:33:51.095Z',
-          Followship: {
-            followerId: 1,
-            followingId: 3,
-            createdAt: '2019-08-01T10:26:55.618Z',
-            updatedAt: '2019-08-01T10:26:55.618Z'
-          }
-        },
-        {
-          id: 2,
-          name: 'user1',
-          email: 'user1@example.com',
-          password:
-            '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
-          isAdmin: false,
-          image: 'https://i.imgur.com/Q14p2le.jpg',
-          createdAt: '2019-07-30T16:24:55.204Z',
-          updatedAt: '2019-08-01T10:34:15.105Z',
-          Followship: {
-            followerId: 2,
-            followingId: 3,
-            createdAt: '2019-08-01T10:27:49.094Z',
-            updatedAt: '2019-08-01T10:27:49.094Z'
-          }
-        }
-      ],
-      FollowerCount: 2,
-      isFollowed: true
-    },
-    {
-      id: 2,
-      name: 'user1',
-      email: 'user1@example.com',
-      password: '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
-      isAdmin: false,
-      image: 'https://i.imgur.com/Q14p2le.jpg',
-      createdAt: '2019-07-30T16:24:55.204Z',
-      updatedAt: '2019-08-01T10:34:15.105Z',
-      Followers: [
-        {
-          id: 1,
-          name: 'root',
-          email: 'root@example.com',
-          password:
-            '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
-          isAdmin: true,
-          image: 'https://i.imgur.com/58ImzMM.png',
-          createdAt: '2019-07-30T16:24:54.983Z',
-          updatedAt: '2019-08-01T10:33:51.095Z',
-          Followship: {
-            followerId: 1,
-            followingId: 2,
-            createdAt: '2019-08-01T10:26:54.947Z',
-            updatedAt: '2019-08-01T10:26:54.947Z'
-          }
-        }
-      ],
-      FollowerCount: 1,
-      isFollowed: true
-    },
-    {
-      id: 1,
-      name: 'root',
-      email: 'root@example.com',
-      password: '$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe',
-      isAdmin: true,
-      image: 'https://i.imgur.com/58ImzMM.png',
-      createdAt: '2019-07-30T16:24:54.983Z',
-      updatedAt: '2019-08-01T10:33:51.095Z',
-      Followers: [
-        {
-          id: 2,
-          name: 'user1',
-          email: 'user1@example.com',
-          password:
-            '$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y',
-          isAdmin: false,
-          image: 'https://i.imgur.com/Q14p2le.jpg',
-          createdAt: '2019-07-30T16:24:55.204Z',
-          updatedAt: '2019-08-01T10:34:15.105Z',
-          Followship: {
-            followerId: 2,
-            followingId: 1,
-            createdAt: '2019-08-01T10:27:45.616Z',
-            updatedAt: '2019-08-01T10:27:45.616Z'
-          }
-        }
-      ],
-      FollowerCount: 1,
-      isFollowed: false
-    }
-  ]
-}
+import usersAPI from '../apis/users'
+import { Toast } from '../utils/helpers'
 
 export default {
   components: {
@@ -142,8 +29,20 @@ export default {
     this.fetchUser()
   },
   methods: {
-    fetchUser() {
-      this.users = dummyData.users
+    async fetchUser() {
+      try {
+        const { data, statusText } = await usersAPI.getTopUsers()
+
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+        this.users = data.users
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得美食達人，請稍後再試'
+        })
+      }
     }
   }
 }
